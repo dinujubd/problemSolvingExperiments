1353daac681711174557172b29f50fc0dcce82c7
Dinar <maildinuju@gmai.com>
2020-08-31 07:13:53 +0700

    max average size array

---
+++ sliding window/maxmum_size_array_easy_sliding.js
@@ -0,0 +1,23 @@
+const max_sub_array_of_size_k = (k, arr) => {
+
+    let windowStart, windowSum = 0;
+    let result = [];
+
+    for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+        windowSum += arr[windowEnd]
+
+        if (windowEnd >= k - 1) {
+            result.push(windowSum / k);
+            windowSum -= arr[windowStart]
+            windowStart += 1;
+        }
+    }
+
+    return result;
+};
+
+const solution = () => console.log(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5]))
+solution();
+module.exports = solution
+
+
