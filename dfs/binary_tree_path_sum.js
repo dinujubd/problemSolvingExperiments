777e75778c1034455235f736973ea2e4e034c4b6
Dinar <maildinuju@gmai.com>
2020-09-09 09:13:40 +0700

    is possible sum

---
+++ dfs/binary_tree_path_sum.js
@@ -0,0 +1,29 @@
+class TreeNode {
+
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+};
+
+
+const has_path = function (root, sum) {
+
+    if (root === null) return false;
+    if (root.left === null && root.right === null && sum === root.value) return true;
+
+    return has_path(root.left, sum - root.value) || has_path(root.right, sum - root.value)
+};
+
+
+var root = new TreeNode(12)
+root.left = new TreeNode(7)
+root.right = new TreeNode(1)
+root.left.left = new TreeNode(9)
+root.right.left = new TreeNode(10)
+root.right.right = new TreeNode(5)
+console.log(`Tree has path: ${has_path(root, 23)}`)
+console.log(`Tree has path: ${has_path(root, 16)}`)
+
+module.exports = has_path
