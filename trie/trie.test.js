f4e7d07a3b502f47ffbacd285030c5cc4e5838fa
Dinar <maildinuju@gmai.com>
2020-12-13 21:37:15 +0700

    added tests

--- trie/trie.test.js
+++ trie/trie.test.js
@@ -2,19 +2,38 @@ const Trie = require("./trie")
 
 
 describe("Trie", () => {
-    it("should insert properly", () => {
-        const trie = Trie();
-        trie.insert("world")
-        const isStarted = trie.startsWith("wor");
-        const isNotStartedWith = trie.startsWith("abc");
-        const hasWholeWord = trie.search("world");
-        const hasNoCompleteWord = trie.search("wor");
-        const hasNoCompeteWord2 = trie.search("abc");
+    let trie = null;
+    beforeEach(() => {
+        trie = Trie();
+    });
 
-        expect(isStarted).toBeTruthy();
-        expect(isNotStartedWith).toBeFalsy();
-        expect(hasWholeWord).toBeTruthy();
-        expect(hasNoCompleteWord).toBeFalsy();
-        expect(hasNoCompeteWord2).toBeFalsy();
-    })
+    afterEach(() => {
+        trie = null;
+    });
+
+    it("should insert properly", () => {
+        trie.insert("world");
+
+        expect(trie.startsWith("wor")).toBeTruthy();
+        expect(trie.startsWith("abc")).toBeFalsy();
+        expect(trie.search("world")).toBeTruthy();
+        expect(trie.search("wor")).toBeFalsy();
+        expect(trie.search("abc")).toBeFalsy();
+    });
+
+    it("should remove entire string for lonely branch properly", () => {
+        trie.insert("work");
+        trie.remove("work");
+
+        expect(trie.search("work")).toBeFalsy();
+    });
+
+    it("should remove only the tag for word if there are dependent words", () => {
+        trie.insert("work");
+        trie.insert("worked");
+        trie.remove("work");
+
+        expect(trie.startsWith("work")).toBeTruthy();
+        expect(trie.search("work")).toBeFalsy();
+    });
 })
