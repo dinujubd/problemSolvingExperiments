14a2bd0f41a901a3929219d6e8195c055681b305
Dinar <maildinuju@gmai.com>
2020-12-13 21:37:05 +0700

    added delete functionaliity

--- trie/trie.js
+++ trie/trie.js
@@ -1,13 +1,18 @@
 
 const Trie = () => {
-    const root = {};
+    const root = { char: '\0', children: {}, isWord: false };
 
     const insert = (word) => {
         let current = root;
 
         for (var i = 0; i < word.length; i++) {
+            const c = word[i];
 
-            current = current[word[i]] = current[word[i]] || {}
+            if (!(c in current.children)) {
+                current.children[c] = { char: c, children: {}, isWord: false }
+            }
+
+            current = current.children[c];
         }
 
         current.isWord = true;
@@ -23,16 +28,35 @@ const Trie = () => {
         return !!n;
     };
 
+    const remove = (word, node = root, dep = 0) => {
+        if (word.length === dep) {
+            if (Object.keys(node.children).length === 0) {
+                return true;
+            } else {
+                node.isWord = false;
+                return false;
+            }
+        } else {
+            const nextNode = node.children[word[dep]];
+            if (remove(word, nextNode, dep + 1)) {
+                delete node.children[word[dep]]
+                return Object.keys(node.children).length === 0;
+            }
+        }
+
+        return false;
+    }
+
     const getNode = (word) => {
         let current = root;
         for (var i = 0; i < word.length; i++) {
             if (!current) return null;
-            current = current[word[i]];
+            current = current.children[word[i]];
         }
         return current;
     }
 
-    return { insert, search, startsWith };
+    return { insert, search, startsWith, remove };
 
 };
 
