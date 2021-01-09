a968f2aa3c45d10bb4e4d9dd572d600aca3bc845
Dinar <maildinuju@gmail.com>
2021-01-09 17:55:24 +0700

    fixed n repeat substring

--- educative.io/sliding_window/no_repeat_substring.js
+++ educative.io/sliding_window/no_repeat_substring.js
@@ -2,16 +2,14 @@ const no_repeat_substring = function (str) {
     let maxSubstr = -1;
     if (!str) return maxSubstr;
     let minWindow = 0;
-    let setOfChars = new Set();
+    let setOfChars = {};
 
     for (let maxWindow = 0; maxWindow < str.length; maxWindow++) {
-        if (setOfChars.has(str[maxWindow])) {
-            minWindow++;
-            maxSubstr = Math.max(maxSubstr, maxWindow - minWindow + 1);
-        } else {
-            setOfChars.add(str[maxWindow])
-
+        if (str[maxWindow] in setOfChars) {
+            minWindow = Math.max(minWindow, setOfChars[str[maxWindow]] + 1);
         }
+        setOfChars[str[maxWindow]] = maxWindow;
+        maxSubstr = Math.max(maxSubstr, maxWindow - minWindow + 1);
     }
 
     return maxSubstr;
--- educative.io/sliding_window/tests/no_repeat_substring.test.js
+++ educative.io/sliding_window/tests/no_repeat_substring.test.js
@@ -2,7 +2,7 @@ const solution = require('../no_repeat_substring')
 
 describe('no_repeat_substring',() => {
     it('Should work', () => {
-        const  maNoRepeatSubstring = solution("aabccbb");
+        const  maNoRepeatSubstring = solution("pwwkew");
         expect(maNoRepeatSubstring).toBe(3);
     });
 })
